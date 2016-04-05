1b0df030d475e0db74121a5270d7a2b0a98a3f6d
Toan Tran <ttkbnt@hotmail.com>
2016-04-05 18:08:37 -0500

    Added first component

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var RiemannApp = require('./components/Riemann.React');
+
+ReactDOM.render(
+  <RiemannApp />,
+  document.getElementById('appContainer')
+);
